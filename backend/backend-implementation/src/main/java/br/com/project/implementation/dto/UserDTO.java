6751c8817bcc7ed046e0620ddc63294775fff4f5
PedroGabrielM <pedrombalduino@gmail.com>
2023-12-02 14:08:17 -0300

    feat: added excpetion class and create UserDTO

--- backend/backend-implementation/src/main/java/br/com/project/implementation/dto/UserDTO.java
+++ backend/backend-implementation/src/main/java/br/com/project/implementation/dto/UserDTO.java
@@ -1,17 +1,20 @@
-package br.com.project.domain;
+package br.com.project.implementation.dto;
 
+import br.com.project.domain.Type;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
-@NoArgsConstructor
 @AllArgsConstructor
-public class UserModel {
+public class UserDTO {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,8 +23,4 @@ public class UserModel {
     @Getter(AccessLevel.NONE)
     private String password;
     private Type type;
-    private String token;
-
-
 }
-
---
+++ backend/backend-usecases/src/main/java/br/com/project/usecases/exception/InvalidIdException.java
@@ -0,0 +1,4 @@
+package br.com.project.usecases.exception;
+
+public class InvalidIdException extends RuntimeException {
+}
