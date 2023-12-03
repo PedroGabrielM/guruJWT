8eb043b35eb28bf8dd2ea2efc0a8b4e57cfd1189
PedroGabrielM <pedrombalduino@gmail.com>
2023-12-02 21:48:12 -0300

    feat: added frontend spring, ajusted pom

---
+++ frontend/frontend-spring/src/main/java/br/com/project/frontendspring/FrontendSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.project.frontendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendSpringApplication.class, args);
+	}
+
+}
